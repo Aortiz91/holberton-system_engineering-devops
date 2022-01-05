3604b34359447b0b252dcd6cd650981abf93bfc0
Andres Ortiz <3283@holbertonschool.com>
2022-01-04 18:35:17 -0800

    Added ensure

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,14 @@
 # Using Puppet, modify configuration file 
 
 file_line { 'IdentityFile':
-  path  => '/etc/ssh/ssh_config',
-  match => '^IdentityFile',
-  line  => '^IdentityFile ~/.ssh/school',
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  match  => '^IdentityFile',
+  line   => '^IdentityFile ~/.ssh/school',
 }
 
 file_line { 'PasswordAuthentication':
+  ensure = present,
   path  => '/etc/ssh/ssh_config',
   match => '^PasswordAuthentication',
   line  => 'PasswordAuthentication no',
