c54e29ead0d600f1981f5e178f630162a2adeed1
Andres Ortiz <3283@holbertonschool.com>
2022-01-04 18:43:32 -0800

    corrections

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,12 @@
 # Using Puppet, modify configuration file 
 
 file_line { 'IdentityFile':
-  ensure => present,
   path   => '/etc/ssh/ssh_config',
   match  => '^IdentityFile',
-  line   => '^IdentityFile ~/.ssh/school',
+  line   => 'IdentityFile ~/.ssh/school',
 }
 
 file_line { 'PasswordAuthentication':
-  ensure => present,
   path   => '/etc/ssh/ssh_config',
   match  => '^PasswordAuthentication',
   line   => 'PasswordAuthentication no',
