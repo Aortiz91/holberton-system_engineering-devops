c0282dd83b7e065e753352a8014dd40dfc23f39d
Andy <3283@holbertonschool.com>
2022-02-04 09:55:17 -0800

    doc changes

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,34 +1,35 @@
 #!/usr/bin/python3
-"""API DATA TO PRINT"""
+"""Python script that returns information using a REST API"""
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    """Python script that returns information using a REST API"""
     import requests
-    from sys import argv
+    import sys
 
-    todos = 'https://jsonplaceholder.typicode.com/todos'
-    users = 'https://jsonplaceholder.typicode.com/users'
-
-    response1 = requests.get(todos)
-    response2 = requests.get(users)
-
-    users_json = response2.json()
-    todos_json = response1.json()
-
-    name = users_json[int(argv[1]) - 1]['name']
-
-    donetask = 0
-    tasks = 0
-    dosome = []
-    uid = int(argv[1])
-    for tmp in todos_json:
-        if tmp['userId'] == uid:
-            if tmp['completed'] is True:
-                donetask = donetask + 1
-                dosome.append(tmp['title'])
-            tasks = tasks + 1
-
-    print('Employee {} is done with tasks({}/{}):'.format(
-        name, donetask, tasks))
-    for TASK_TITLE in dosome:
-        print('\t {}'.format(TASK_TITLE))
+    url = 'https://jsonplaceholder.typicode.com/users/'
+    empId = sys.argv[1]
+    
+    reqName = requests.get(url + empId)
+    reqTodos = requests.get(url + empId + '/todos/')
+    
+    reqNameJSON = reqName.json()
+    reqTodosJSON = reqTodos.json()
+    
+    total = 0
+    completed = 0
+    tasks = []
+    
+    for elem in (reqTodosJSON):
+        if ((elem['completed']) is True):
+            completed += 1
+            total += 1
+            tasks.append(elem['title'])
+        else:
+            total += 1
+    
+    print('Employee {} is done with tasks ({}/{}):'.format(
+          (reqNameJSON).get('name'), completed, total))
+    
+    for t in tasks:
+        print(f"\t{t}")
