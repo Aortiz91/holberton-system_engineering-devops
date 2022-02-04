1dcf21aff0915942581cf1df7e39d901fed1a9ad
Andy <3283@holbertonschool.com>
2022-02-04 10:07:38 -0800

    doc changes

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -10,10 +10,10 @@ if __name__ == "__main__":
     import requests
     import sys
 
-    url = 'https://jsonplaceholder.typicode.com/users/'
+    url = "https://jsonplaceholder.typicode.com/users/"
     empId = sys.argv[1]
     urlName = url + empId
-    urlTodos = url + empId + '/todos/'
+    urlTodos = url + empId + "/todos/"
 
     reqName = requests.get(urlName)
     reqTodos = requests.get(urlTodos)
@@ -24,16 +24,16 @@ if __name__ == "__main__":
     total = 0
     completed = 0
     tasks = []
-    name = reqNameJSON.get('name')
+    name = reqNameJSON.get("name")
 
     for elem in (reqTodosJSON):
-        if ((elem['completed']) is True):
+        if ((elem["completed"]) is True):
             completed += 1
             total += 1
-            tasks.append(elem['title'])
+            tasks.append(elem["title"])
         else:
             total += 1
-    print('Employee {} is done with tasks ({}/{}):'.format(
+    print("Employee {} is done with tasks ({}/{}):".format(
           name, completed, total))
     for t in tasks:
         print(f"\t{t}")
