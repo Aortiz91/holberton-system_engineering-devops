fd9b2bd711720f8ba1dda978c876067e1058d0f5
Andy <3283@holbertonschool.com>
2022-02-04 17:35:56 -0800

    Added 3

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -7,23 +7,6 @@ import sys
 if __name__ == "__main__":
 
     url = "https://jsonplaceholder.typicode.com/users/"
-    reqName = requests.get(url + sys.argv[1])
-    reqTodos = requests.get(url + sys.argv[1] + "/todos/")
-    name = reqName.json().get("name")
-    total = 0
-    comp = 0
-    tasks = []
-    for elem in (reqTodos.json()):
-        if ((elem["completed"]) is True):
-            comp += 1
-            total += 1
-            tasks.append(elem["title"])
-        else:
-            total += 1
-    print("Employee {} is done with tasks({}/{}):".format(name, comp, total))
-    for t in tasks:
-        print("\t {}". format(t))
-
     reqAllTodos = requests.get("https://jsonplaceholder.typicode.com/users/")
     uid = 1
     newList = []
@@ -37,10 +20,12 @@ if __name__ == "__main__":
                 dictTask["completed"] = True
             else:
                 dictTask["completed"] = False
-            dictTask["username"] = (requests.get(url + str(uid))).json().get("username")
+            dictTask["username"] = (
+                    requests.get(url + str(uid))).json().get("username")
             newList.append(dictTask)
+            print(newList)
         newDict[uid] = newList
         uid += 1
-    with open(sys.argv[1] + ".json", "w") as f:
+    with open("todo_all_employees.json", "w") as f:
         f.write(json.dumps(newDict))
         f.close
