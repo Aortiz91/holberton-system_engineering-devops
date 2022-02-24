fab82761916ed46a0213557c0cc78bcd57cdb544
root <root@H62.hbtn.io>
2022-02-24 14:10:49 -0300

    puppet-lint

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,5 +2,5 @@
 
 exec { 'fix limit connections':
         command => 'sed -i s/15/4069/ /etc/default/nginx; sudo service nginx restart',
-        path => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
+        path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
 }
