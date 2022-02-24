e9593db7678920e2a4652d7853a32092b8411ef2
root <root@H62.hbtn.io>
2022-02-24 14:05:59 -0300

    correction

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 #Fix limit connections 
 
 exec { 'fix limit connections':
-        command => 'sed -i \'s/http {/http {\n  open_file_cache max=1024 inactive=10s;\n    open_file_cache_valid 120s;/g\' /etc/nginx/nginx.conf && service nginx restart',
+        command => 'sed -i s/15/4069/ /etc/default/nginx; sudo service nginx restart',
         path => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
 }
